735f2452b54e99577821eb42e63eee244a0d481d
wangdeao <203158558@qq.com>
2021-12-20 19:56:40 +0800

    second commit

--- src/main/java/com/wang/test/test01.java
+++ src/main/java/com/wang/test/test01.java
@@ -3,6 +3,6 @@ package com.wang.test;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("hello git");
-
+        System.out.println("push,test");
     }
 }
